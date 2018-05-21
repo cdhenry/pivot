9f57b2f40add59a17d990790e867c396d028f702
github username <github email address>
2018-05-21 10:58:31 -0400

    added careers Reducer

---
+++ client/src/reducers/careersReducer.js
@@ -0,0 +1,10 @@
+export default function careersReducer(state= {loading: false, careers: []}, action) {
+  switch ( action.type ){
+    case 'LOADING_CAREERS':
+      return Object.assign({}, state,{loading: true})
+    case 'FETCH_CAREERS':
+      return {loading: false, careers: action.careers}
+    default:
+      return state;
+  }
+}
