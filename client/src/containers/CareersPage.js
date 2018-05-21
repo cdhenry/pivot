46b4013bfb784047f26e3b801f9556845c42ede4
github username <github email address>
2018-05-21 10:50:50 -0400

    added careers page container

---
+++ client/src/containers/CareersPage.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCareers } from '../actions/careers';
+import CareerList from '../components/CareerList';
+import { bindActionCreators } from 'redux'
+
+class CareersPage extends Component {
+
+  componentDidMount(){
+    if(this.props.careers.length === 0) {
+     this.props.fetchCareers()
+   }
+  }
+
+  render() {
+    const careers = this.props.careers
+
+    return (
+      <div className="careersContainer" align="center">
+      <br />
+      <h3><strong>All Careers: </strong></h3><br/>
+      <CareerList careers={careers} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {careers: state.careers}
+}
+
+export default connect(mapStateToProps, {fetchCareers})(CareersPage);
