bd742318748fd96d941ebd1c275a18a04554306b
github username <github email address>
2018-05-25 15:41:09 -0400

    added routes for job zones api page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope '/api' do
     get :careers, to: 'careers#index'
     get :career, to: 'careers#search'
+    get :job_zones, to: 'job_zones#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
