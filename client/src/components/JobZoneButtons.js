df5dfaf66934fd6185e3001280115ee43e4c8c03
cdhenry <cdhenry@gmail.com>
2018-05-26 09:56:53 -0400

    created new presontational component for button links to jump to different job zones

---
+++ client/src/components/JobZoneButtons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CareerStyle.css';
+
+function JobZoneButtons() {
+  return(
+    <div className="row justify-content-md-center">
+        <a href="#1" className="btn btn-primary btn-sm ZoneButton">Job Zone 1</a>
+        <a href="#2"  className="btn btn-primary btn-sm ZoneButton">Job Zone 2</a>
+        <a href="#3"  className="btn btn-primary btn-sm ZoneButton">Job Zone 3</a>
+        <a href="#4"  className="btn btn-primary btn-sm ZoneButton">Job Zone 4</a>
+        <a href="#5"  className="btn btn-primary btn-sm ZoneButton">Job Zone 5</a>
+    </div>
+  )
+}
+
+export default JobZoneButtons;
