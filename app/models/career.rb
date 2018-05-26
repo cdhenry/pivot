3bec1c54c8212c359871e36545372464eb9feb0d
cdhenry <cdhenry@gmail.com>
2018-05-26 17:10:56 -0400

    created methods to parse individual career data

--- app/models/career.rb
+++ app/models/career.rb
@@ -1,17 +1,23 @@
 class Career < ActiveRecord::Base
   belongs_to :job_zone
   has_many :predictors
-  # def self.search(career_title)
-  #   conn = Faraday.new 'https://services.onetcenter.org'
-  #   conn.basic_auth(ENV['ONET_USERNAME'], ENV['ONET_PASSWORD'])
-  #   resp = conn.get(`/ws/mnm/search?keyword=#{career_title}`, {}, {'Accept' => 'application/json'})
-  #   body_hash = JSON.parse(resp.body)
-  # end
-  #
-  # def self.allData
-  #   conn = Faraday.new 'https://services.onetcenter.org'
-  #   conn.basic_auth(ENV['ONET_USERNAME'], ENV['ONET_PASSWORD'])
-  #   resp = conn.get('/ws/mnm/careers', {}, {'Accept' => 'application/json'})
-  #   body_hash = JSON.parse(resp.body)
-  # end
+
+  def self.search(career_url)
+    conn = Faraday.new
+    conn.basic_auth(ENV['ONET_USERNAME'], ENV['ONET_PASSWORD'])
+    resp = conn.get(career_url, {}, {'Accept' => 'application/json'})
+    body_hash = JSON.parse(resp.body)
+    convert_career_hash(body_hash)
+  end
+
+  def self.convert_career_hash(body_hash)
+    aka = ""
+    tasks = ""
+    body_hash["also_called"]["title"].each{|title| aka = aka + ", " + title}
+    body_hash["on_the_job"]["task"].each{|task| tasks = tasks + " " + task}
+    result = body_hash.except("resources", "tags", "code", "also_called", "on_the_job")
+    result["aka"] = aka.slice(2..-1)
+    result["on_the_job"] = tasks.slice(1..-1)
+    result
+  end
 end
