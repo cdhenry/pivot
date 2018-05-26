d8e4fe0659513424aa5bd8542330d5345c333c64
cdhenry <cdhenry@gmail.com>
2018-05-26 17:11:48 -0400

    created career reducer for individual career fetch

---
+++ client/src/reducers/careerReducer.js
@@ -0,0 +1,10 @@
+export default function careerReducer(state= {loading: false, career: []}, action) {
+  switch ( action.type ){
+    case 'LOADING_CAREER':
+      return Object.assign({}, state,{loading: true})
+    case 'FETCH_CAREER':
+      return {loading: false, career: action.career}
+    default:
+      return state;
+  }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
+import careerReducer from './careerReducer'
 import careersReducer from './careersReducer'
 import jobZonesReducer from './jobZonesReducer'
 
 const rootReducer = combineReducers({
+  career: careerReducer,
   careers: careersReducer,
   jobZones: jobZonesReducer
 });
