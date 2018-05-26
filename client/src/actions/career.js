e2ee45a4ac3c2c20e1be94e5f9118dbf04aa1809
cdhenry <cdhenry@gmail.com>
2018-05-26 17:09:45 -0400

    added career fetch action

---
+++ client/src/actions/career.js
@@ -0,0 +1,13 @@
+import fetch from 'isomorphic-fetch';
+
+export function fetchCareer(id) {
+  return function(dispatch) {
+    dispatch({type: 'LOADING_CAREER'});
+    return fetch('/api/careers/' + id)
+    .then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      return dispatch({type: 'FETCH_CAREER', career: responseJSON})
+    })
+  }
+}
