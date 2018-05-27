d9f12c36c2e3c64000752ad4487a6f099cadbb07
cdhenry <cdhenry@gmail.com>
2018-05-27 18:36:30 -0400

    added potential career show presontational component

---
+++ client/src/components/CareerShow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CareerShow  = ({career}) => {
+  // const renderCareer = career.map((career) =>
+  //
+  // );
+
+  return(
+    <div className="row justify-content-md-center">
+
+    </div>
+  )
+}
+
+export default CareerShow;
