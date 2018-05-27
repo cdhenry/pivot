82d2d1f6896c47b4232208737b9a6cfec819814f
cdhenry <cdhenry@gmail.com>
2018-05-27 18:39:13 -0400

    playing with presontation options for career data

--- client/src/containers/CareerPage.js
+++ client/src/containers/CareerPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCareer } from '../actions/career';
+import CareerShow from '../components/CareerShow';
 
 class CareerPage extends Component {
 
   componentDidMount(){
-    debugger;
     if(this.props.career.length === 0) {
       this.props.fetchCareer(this.props.match.params.careerId)
     }
@@ -16,7 +16,7 @@ class CareerPage extends Component {
 
     return (
       <div className="container" align="center">
-        {career}
+        {career.title}, {career.aka}, {career.on_the_job}, {career.what_they_do}
       </div>
     );
   }
@@ -27,22 +27,3 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, {fetchCareer})(CareerPage);
-
-// ({ career }) =>
-//   <div>
-//     <h2>{career.title}</h2>
-//   </div>;
-//
-// const mapStateToProps = (state, ownProps) => {
-//   debugger;
-//   fetchCareer(ownProps.match.params.careerId)
-//   const career = state.careers.careers.find(career => career.id === +ownProps.match.params.careerId)
-//
-//   if (career) {
-//     return { career }
-//   } else {
-//     return { career: {} }
-//   }
-// };
-//
-// export default connect(mapStateToProps)(CareerShow);
