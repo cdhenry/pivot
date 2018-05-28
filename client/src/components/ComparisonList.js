bd4d9b4e640032c7322ba2286fb28105bb7fb415
cdhenry <cdhenry@gmail.com>
2018-05-28 08:44:20 -0400

    styled comparison show page

--- client/src/components/ComparisonList.js
+++ client/src/components/ComparisonList.js
@@ -1,9 +1,47 @@
 import React from 'react';
-//import '../styles/ComparisonStyle.css';
+import '../styles/CompareStyle.css';
 
 const ComparisonList = ({comparisons}) => {
+  debugger;
   const renderComparisons = comparisons.map((comparison, index) =>
-    <div>{comparison.id}</div>
+    <div>
+      <div className="card-header">Comparison {comparison.id}</div>
+      <table className="TableShowStyle table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Predictors</th>
+            {comparison.careers.map((career) => <th scope="col">{career.title}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="table-primary">
+            <th scope="row">Engaging Work</th>
+            {comparison.predictors.map((predictor) => <th scope="col">{predictor.engagement}</th>)}
+          </tr>
+          <tr className="table-light">
+            <th scope="row">Work that helps others</th>
+            {comparison.predictors.map((predictor) => <th scope="col">{predictor.altruism}</th>)}
+          </tr>
+          <tr className="table-primary">
+            <th scope="row">Being good at your job</th>
+            {comparison.predictors.map((predictor) => <th scope="col">{predictor.skill}</th>)}
+          </tr>
+          <tr className="table-light">
+            <th scope="row">Supportive Colleagues</th>
+            {comparison.predictors.map((predictor) => <th scope="col">{predictor.support}</th>)}
+          </tr>
+          <tr className="table-primary">
+            <th scope="row">Basic Needs</th>
+            {comparison.predictors.map((predictor) => <th scope="col">{predictor.basic_needs}</th>)}
+          </tr>
+          <tr className="table-light">
+            <th scope="row">Fit with rest of life</th>
+            {comparison.predictors.map((predictor) => <th scope="col">{predictor.balance}</th>)}
+          </tr>
+        </tbody>
+      </table>
+      <br/>
+    </div>
   );
 
   return(
